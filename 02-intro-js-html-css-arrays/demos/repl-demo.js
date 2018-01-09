ef7a0830b5946768913ee37d3bbdf4d60ee7affd
Allie <allie@codefellows.com>
2018-01-09 12:40:20 -0800

    adding lab instructions, new repo instructions, and repl demo

---
+++ 02-intro-js-html-css-arrays/demos/repl-demo.js
@@ -0,0 +1,45 @@
+var string = 'This is a string';
+var string2 = '2';
+var num = 2;
+
+
+
+// DATA TYPES
+// Strings
+// Numbers
+// Booleans
+// console.log(typeof(num));
+
+// var userAge = prompt('How old are you?');
+// console.log(userAge);
+// console.log('before parseInt', typeof(userAge));
+// userAge = parseInt(userAge);
+// console.log('after parseInt', typeof(userAge));
+
+
+// ARRAYS - collections, store more than one value in a variable as an array, data structure
+
+var arrayOfDogs = ['Gary', 'Charlotte', 'Demi', 'Bird', 'Mr. Darcy'];
+arrayOfDogs[5] = 'Whiskey';
+arrayOfDogs[5] = 'Holly';
+console.log(arrayOfDogs.length);
+arrayOfDogs.push('Toto');
+arrayOfDogs.pop();
+arrayOfDogs.pop();
+arrayOfDogs.length;
+arrayOfDogs.unshift('Fluffy');
+console.log(arrayOfDogs);
+arrayOfDogs.shift();
+console.log(arrayOfDogs);
+
+
+
+
+
+
+
+
+
+
+
+
