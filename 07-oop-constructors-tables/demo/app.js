59d3935f79fcc1c5ad8dd66b46bb3108c3e50c77
Allie <allie@codefellows.com>
2018-01-16 12:08:54 -0800

    adding demo and lab.md for day 7

--- 07-oop-constructors-tables/demo/app.js
+++ 07-oop-constructors-tables/demo/app.js
@@ -25,29 +25,80 @@ Thus, we'll want to create and append HTML elements in the same order.
 We have already put an empty <table> element in the HTML, with id="dogs" */
 
 // We need an array to hold our dogs
+var allDogs = [];
 
 // We need to access the table that is in the DOM
+var dogTable = document.getElementById('dogs');
 
 // We need a constructor to make our dog objects
+function Dog(name, color, breed, nickname) {
+  this.name = name;
+  this.color = color;
+  this.breed = breed;
+  this.nickname = nickname;
+  allDogs.push(this);
+}
 
 // Let's refactor so that render() method is on the constructor's prototype; this will tidy up the way things look on the screen
-render = function () {
+Dog.prototype.render = function () {
   // create tr
-
+  var trEl = document.createElement('tr');
   // create td
-
+  var tdEl = document.createElement('td');
   // give td content (name, then color, then breed, the nickname) <- eventually do it in a for loop
-
+  tdEl.textContent = this.name;
   // append td to tr
-
+  trEl.appendChild(tdEl);
   // append tr to table
+  
+  tdEl = document.createElement('td');
+  tdEl.textContent = this.color;
+  trEl.appendChild(tdEl);
 
+  tdEl = document.createElement('td');
+  tdEl.textContent = this.breed;
+  trEl.appendChild(tdEl);
+
+  tdEl = document.createElement('td');
+  tdEl.textContent = this.nickname;
+  trEl.appendChild(tdEl);
+  
+  dogTable.appendChild(trEl);
 };
 
-// We need to create our Dog instances
-
 // We need a separate function to make the table header
+function makeHeaderRow() {
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Name';
+  trEl.appendChild(thEl);
 
-// It would be nice to have a single function that renders all of the individual dog rows
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Color';
+  trEl.appendChild(thEl);
+
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Breed';
+  trEl.appendChild(thEl);
+
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Nickname';
+  trEl.appendChild(thEl);
+
+  dogTable.appendChild(trEl);
+}
+
+// We need to create our Dog instances
+var gary = new Dog('Gary', 'White', 'Westie', 'Gare Bear');
+var charlotte = new Dog('Charlotte', 'White', 'Westie', 'Goose');
+var ollie = new Dog('Ollivander', 'Tan', 'French Bulldog', 'Ollie');
+var buddy = new Dog('Buddy', 'Black', 'Labra-doodle', '');
+var demi = new Dog('Demi', 'Black and White', 'Border Collie', null);
 
 // Now we need to call our functions: the one for the header row, and the one for generating the individual dog rows
+makeHeaderRow();
+gary.render();
+charlotte.render();
+ollie.render();
+buddy.render();
+demi.render();
